094606d9b34c24cf47fac932dfdc4339ecf1bee7
gustavogavancho <ggavancho@deepdev.com.mx>
2020-10-12 10:51:17 -0500

    Primer paso

--- MyNiubizApp/MyNiubizApp/App.xaml.cs
+++ MyNiubizApp/MyNiubizApp/App.xaml.cs
@@ -2,16 +2,41 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using MyNiubizApp.Views;
+using Visanet.Controllers;
 
 namespace MyNiubizApp
 {
     public partial class App : Application
     {
-
+        public static ConfigurationController configuration = new ConfigurationController();
         public App()
         {
             InitializeComponent();
             MainPage = new AppShell();
+
+            //Environment
+            configuration.Environment.Protocol = "https://";
+            configuration.Environment.Host = "apitestenv.vnforapps.com/";
+
+            //Session
+            configuration.Session.amount = 10;
+            configuration.Session.merchantId = "341198210";
+
+            //Credentials
+            configuration.Credential.User = "integraciones.visanet@necomplus.com";
+            configuration.Credential.Password = "d5e7nk$M";
+
+            //SSL Certificate
+            configuration.BackendPublicKey = "";
+
+            //Secutiry
+            configuration.Security.accessToken = "aprcZ2lhbmNhZ2FsbGFyZG9AZ21haWwuY29tOkF2MyR0cnV6";
+
+            //Antifraud
+            configuration.Ecommerce.antifraud.merchantDefineData.Add("MDD8", "TempMDD8");
+            configuration.Ecommerce.antifraud.merchantDefineData.Add("MDD50", "TempMDD50");
+            configuration.Ecommerce.antifraud.merchantDefineData.Add("MDD40", "TempMDD40");
+            configuration.Ecommerce.antifraud.merchantDefineData.Add("MDD20", "TempMDD20"); 
         }
 
         protected override void OnStart()
--- MyNiubizApp/MyNiubizApp/ViewModels/CreditCardPageViewModel.cs
+++ MyNiubizApp/MyNiubizApp/ViewModels/CreditCardPageViewModel.cs
@@ -1,9 +1,13 @@
-﻿using System.ComponentModel;
+﻿using MyNiubizApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MyNiubizApp.ViewModels
 {
     public class CreditCardPageViewModel : BaseViewModel
     {
+        public Command TestCommand { get; }
+
         public string CardNumber { get; set; }
         public string CardCvv { get; set; }
         public string CardExpirationDate { get; set; }
@@ -11,6 +15,22 @@ namespace MyNiubizApp.ViewModels
         public CreditCardPageViewModel()
         {
             Title = "Pagar compra";
+            TestCommand = new Command(OnTest);
+        }
+
+        private void OnTest(object obj)
+        {
+            var result5 = App.configuration.GetSecurityAsync();
+            var result6 = App.configuration.GetSecurityAsyncWhitAuthorization();
+            var result7 = App.configuration.GetSecurityAsyncWhitAuthorizationAll("aW50ZWdyYWNpb25lcy52aXNhbmV0QG5lY29tcGx1cy5jb206ZDVlN25rJE0=");
+            var result8 = App.configuration.GetSecurityFromConfiguration();
+            var result = App.configuration.CreateSessionAsync();
+            var result1 = App.configuration.AuthorizeECommerceAsync();
+            var result2 = App.configuration.CreateTransactionTokenAsync();
+            var result3 = App.configuration.GetConfigurationMerchant();
+            var result4 = App.configuration.GetQueryBin("340000");
+            App.configuration.SetTrustDefender();
+            //var result9 = App.configuration.LoadFromXaml(nameof(CreditCardPage));
         }
     }
 }
